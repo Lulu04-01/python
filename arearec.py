812483946e4c5910d26bb1b04fb7c22bf4b22d19
LilianaSua <lsua0sena@gmail.com>
2025-10-22 20:24:51 -0500

    Ejercicio 1 y 2 resueltos. Promedio y area de rectangulo

---
+++ arearec.py
@@ -0,0 +1,6 @@
+a = float(input("Ingrese la altura del rectangulo: "))
+b = float(input("Ingrese la base del rectangulo: "))
+
+area = a * b
+
+print("El area del rectangulo es: ", area)
---
+++ promedio.py
@@ -0,0 +1,9 @@
+c1 = float(input("Ingrese la nota 1: "))
+c2 = float(input("Ingrese la nota 2: "))
+c3 = float(input("Ingrese la nota 3: "))
+c4 = float(input("Ingrese la nota 4: "))
+
+suma = c1 + c2 + c3 + c4
+promedio = suma / 4
+
+print("El promedio de las calificaciones es: ", promedio)
